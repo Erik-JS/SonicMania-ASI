3db17e7c0d8daed9d1b1d5bc3a5c23a8184ef327
Erik JS <erikjs_br@hotmail.com>
2018-11-04 19:39:08 -0200

    [LSE/PD] 1.04 compatibility changes

--- SMLevelSelectEnabler/main.cpp
+++ SMLevelSelectEnabler/main.cpp
@@ -1,8 +1,8 @@
 #include <Windows.h>
 
-BYTE pattern1[] = { 0xF6, 0x80, 0xB4, 0x10, 0x04, 0x00, 0x01, 0x74, 0x2E };
+BYTE pattern1[] = { 0xF6, 0x81, 0xB4, 0x10, 0x04, 0x00, 0x01, 0x74, 0x3F };
 
-BYTE pattern2[] = { 0x83, 0x78, 0x54, 0x01, 0x75, 0x1D };
+// BYTE pattern2[] = { 0x83, 0x78, 0x54, 0x01, 0x75, 0x1D };
 
 
 bool DataCompare(const BYTE* OpCodes, const BYTE* Mask, const char* StrMask)
@@ -36,22 +36,14 @@ DWORD WINAPI Start(LPVOID lpParam)
 {
 	Sleep(2100);
 	DWORD target1 = FindPattern(0x400000, 0x500000, pattern1, "xxxxxxxxx", 0);
-	DWORD target2 = FindPattern(0x400000, 0x500000, pattern2, "xxxxxx", 0);
-	if (target1==NULL || target2 == NULL)
+	if (target1==NULL)
 		return 0;
 
 	DWORD dwProtect;
-
 	BYTE *b = (BYTE*)(target1 + 7);
 	VirtualProtect((void*)b, 0x2, PAGE_READWRITE, &dwProtect);
-	*b++ = 0x90;
-	*b-- = 0x90;
-	VirtualProtect((void*)b, 0x2, dwProtect, &dwProtect);
-
-	b = (BYTE*)(target2 + 4);
-	VirtualProtect((void*)b, 0x2, PAGE_READWRITE, &dwProtect);
-	*b++ = 0x90;
-	*b-- = 0x90;
+	*b++ = 0xEB;
+	*b-- = 0x11;
 	VirtualProtect((void*)b, 0x2, dwProtect, &dwProtect);
 
 	return 0;
--- SMPowerDown/main.cpp
+++ SMPowerDown/main.cpp
@@ -1,21 +1,19 @@
 #include <Windows.h>
 
-BYTE pattern[] = { 0xC7, 0x87, 0x24, 0x01, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x5F, 0x8B, 0xE5, 0x5D, 0xC3,
- 0x33, 0xC0, 0x5F, 0x8B, 0xE5, 0x5D, 0xC3 };
-
 __declspec(naked) void PowerDownFunction()
 {
 	__asm
 	{
-		cmp dword ptr [edi+0x124], 0x00000002
+		cmp dword ptr [esi+0x124], 0x00000002
+		jne supercheckdone
+		mov dword ptr [esi+0x124], 0x00000003
+supercheckdone:
+		mov eax, 0x004C85F4
+		cmp ebx,0xFF
 		jne endoffunction
-		mov dword ptr [edi+0x124], 0x00000003
+		mov eax, 0x004C830D
 endoffunction:
-		xor eax, eax
-		pop edi
-		mov esp, ebp
-		pop ebp
-		ret
+		jmp eax
 	}
 }
 
@@ -49,20 +47,16 @@ DWORD FindPattern(DWORD StartAddress, DWORD CodeLen, BYTE* Mask, char* StrMask, 
 DWORD WINAPI Start(LPVOID lpParam)
 {
 	Sleep(2250);
-	DWORD codeLoc = FindPattern(0x400000, 0x500000, pattern, "xxxxxxxxxxxxxxxxxxxxxx", 0);
-	if (!codeLoc)
-		return 0;
-	codeLoc += 15;
+	DWORD codeLoc = 0x004C8301;
 	DWORD codeReturn = codeLoc + 5;
 	DWORD dwProtect;
-	VirtualProtect((void*)codeLoc, 0x7, PAGE_READWRITE, &dwProtect);
+	VirtualProtect((void*)codeLoc, 0x6, PAGE_READWRITE, &dwProtect);
 	BYTE *b = (BYTE*)codeLoc;
 	*b++ = 0xE9;
 	*(DWORD*)b = (DWORD)&PowerDownFunction - codeReturn;
 	b += 4;
-	*b++ = 0x90;
 	*b = 0x90;
-	VirtualProtect((void*)codeLoc, 0x7, dwProtect, &dwProtect);
+	VirtualProtect((void*)codeLoc, 0x6, dwProtect, &dwProtect);
 	return 0;
 }
 
